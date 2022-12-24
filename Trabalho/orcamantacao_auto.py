93a5d4412eb5d36894ca2904bf29bf99aab37034
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2022-12-24 15:21:29 +0000

    teste

--- Trabalho/orcamantacao_auto.py
+++
@@ -1,4 +0,0 @@
-from openpyxl import workbook, load_workbook                    #Importação do modulo de edição de excel
-
-x = load_workbook('orcamentacao_auto.xlsx')                     #
-xs = x.active
