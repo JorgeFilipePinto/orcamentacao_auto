8a3e87fd202bb335c3cd71c809e8c920c749de33
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2022-12-25 14:39:00 +0000

    Estrutura

---
+++ Trabalho/orcamantacao_auto.py
@@ -0,0 +1,58 @@
+# import jason file, para pré enchimendo da nossa base de dados, utilizar o PANDAS
+import json
+
+with open('dados.json', 'r') as f:
+    data_base = json.load(f)
+print(data_base)
+
+#apresentação dos items desponiveis
+
+
+
+#criação de segunda base de dados para acumulação de items adquiridos
+
+
+
+#apresentação do pré folheto em terminal
+print('________________________________________________________________________________')
+print('|                                                                               |')
+print('|                         Escola Superior de Gestão e Tecnologia                |')
+print('|-------------------------------------------------------------------------------|')
+print('|                                   Orçamento                                   |')
+print('|-------------------------------------------------------------------------------|')
+print('|    Item1                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|    Item2                                                                      |')
+print('|                                                                               |')
+print('|                                                                               |')
+print('|                                                   Total S/ IVA:               |')
+print('|                                                            IVA:               |')
+print('|                                                  Total a Pagar:               |')
+print('|                                                                               |')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#exportar para PDF o orçamento possivelmente com o PANDAS
