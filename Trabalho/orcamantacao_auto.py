4daa56cdefea8d153f78e839525b637c67e3eeea
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2022-12-25 14:51:07 +0000

    Estrutura

--- Trabalho/orcamantacao_auto.py
+++ Trabalho/orcamantacao_auto.py
@@ -10,37 +10,39 @@ print(data_base)
 
 
 #criação de segunda base de dados para acumulação de items adquiridos
-
+#ciclo while lê o elemennto do dic guarda em variavel, usa-se a variavel 
+# como fonte de apresentação do nome na input do utilizador
+#Quando o utilizador inserir o valor o mesmo é guardado numa variavel correspondente
 
 
 #apresentação do pré folheto em terminal
-print('________________________________________________________________________________')
-print('|                                                                               |')
-print('|                  Escola Superior de Gestão e Tecnologia de Lamego             |')
-print('|-------------------------------------------------------------------------------|')
-print('|                                   Orçamento                                   |')
-print('|-------------------------------------------------------------------------------|')
-print('|    Item1                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|    Item2                                                  ', Variavel,     '  |')
-print('|                                                                               |')
-print('|                                                                               |')
-print('|                                                   Total S/ IVA:               |')
-print('|                                                            IVA:               |')
-print('|                                                  Total a Pagar:               |')
-print('|_______________________________________________________________________________|')
+print('_____________________________________________________________________________')
+print('|                                                                            |')
+print('|                Escola Superior de Gestão e Tecnologia de Lamego            |')
+print('|----------------------------------------------------------------------------|')
+print('|                                 Orçamento                                  |')
+print('|----------------------------------------------------------------------------|')
+print('|    Item1                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item                                                ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|    Item2                                               ', Variavel,     '  |')
+print('|                                                                            |')
+print('|                                                                            |')
+print('|                                                Total S/ IVA:', Variavel, ' |')
+print('|                                                         IVA:', Variavel, ' |')
+print('|                                               Total a Pagar:', Variavel, ' |')
+print('|____________________________________________________________________________|')
 
 
 
